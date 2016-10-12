5e962077be6a619bb236483bb2c6d6d2524ed570
lbertoni <laismessiasbertoni@hotmail.com>
2016-10-12 15:32:34 -0300

    Ajuste nos testes

--- test/controllers/ideia_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class IdeiaControllerTest < ActionDispatch::IntegrationTest
-  test "should get cadastro" do
-    get ideia_cadastro_url
-    assert_response :success
-  end
-
-end
